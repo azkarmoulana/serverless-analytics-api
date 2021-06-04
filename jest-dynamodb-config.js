e59145f06affc4800386a9b3f79e6d3caf1635e1
azkarmoulana <azkar.moulana@gmail.com>
2021-06-04 22:54:32 +0530

    basic project setup

---
+++ jest-dynamodb-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  tables: [
+    {
+      TableName: 'MetricsTable-dev',
+      KeySchema: [
+        {
+          AttributeName: 'id',
+          KeyType: 'HASH',
+        },
+      ],
+      AttributeDefinitions: [
+        {
+          AttributeName: 'id',
+          AttributeType: 'S',
+        },
+      ],
+      BillingMode: 'PAY_PER_REQUEST',
+    },
+  ],
+};
